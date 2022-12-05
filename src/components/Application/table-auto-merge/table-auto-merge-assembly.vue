3f81cf216e9228e092ec5fcc56bd7a14d3dfca99
leixiaopeng <2672947175@qq.com>
2022-12-05 21:05:51 +0800

    开发公共表格与自动合并功能

---
+++ src/components/Application/table-auto-merge/table-auto-merge-assembly.vue
@@ -0,0 +1,146 @@
+
+<template>
+
+<div>
+    <el-table ref="tableDD4" id="tableDD4" :data="tableData1" :span-method="objectSpanMethod" max-height="600" border>
+      <template e v-for="(item, index) in arrlist" :key="index" >
+        <el-table-column :label="item.label" :prop="item.key" :align="item?.align" :width="item?.width" />
+      </template>
+      <el-table-column  label="操作" width="150">
+          <template #default="scope">
+            <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
+              >编辑</el-button
+            >
+            <el-popconfirm
+              confirmButtonText="确定"
+              cancelButtonText="取消"
+              icon="el-icon-info"
+              iconColor="red"
+              title="确定要删除吗？"
+              @confirm="handleDelete(scope.$index, scope.row)"
+            >
+              <template #reference>
+                <el-button  size="mini" type="danger">删除</el-button>
+              </template>
+            </el-popconfirm>
+          </template>
+        </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:["arrlist","data","tableautomerge"],
+    data() {
+      return {
+        isApplicationAdd:false,
+        // 存储处理后的数据
+        tableData1: [],
+        //存储每一列合并行以及下标数据
+        margeArray: [],
+      }
+     
+    },
+    mounted(){
+      // 校验是否需要表格自动合并
+      if(!!this.tableautomerge&&this.tableautomerge.length){
+         // 数据处理排列合并
+        this.tableData1 =  this.data.map(item=>{
+          let obj = {}
+          this.arrlist.forEach((key, index)=>{
+             obj[key.key]=item[key.key];
+          })
+          return {...obj,...item}
+        })
+
+        let objSort = {}
+        let listSort = []
+        // 数据排序归类
+        this.tableData1.map(item=>{
+          if(!!objSort[item.type]){
+            objSort[item.type]=[...objSort[item.type],item]
+          }else{
+            objSort[item.type] = [item]
+          }
+        })
+        Object.keys(objSort).map(item=>{
+          listSort = [...listSort,...objSort[item]]
+        })
+        this.tableData1 =listSort
+
+        //开始 调用方法计算需要合并的数据
+        for (let i = 0; i < Object.keys(this.tableData1[0]).length; i++) {
+          // 首先添加一个存放合并行数据的变量
+          this.margeArray.push({ Arr: [], Position: 0, })
+          // 得到下标对应的key值
+          const element = Object.keys(this.tableData1[0])[i];
+          // 调用合并，
+          this.rowspan(this.tableData1, this.margeArray[i].Arr, this.margeArray[i].Position, element);
+        }
+      }else{
+        this.tableData1 = this.data
+      }
+    },
+    methods: {
+      rowspan(tableData, spanArr, position, spanName) {
+        tableData.forEach((item, index) => {
+          if (index === 0) {
+            spanArr.push(1);
+            position = 0;
+          } else {
+
+            if (tableData[index][spanName] === tableData[index - 1][spanName]) {
+              spanArr[position] += 1;
+              spanArr.push(0);
+            } else {
+              spanArr.push(1);
+              position = index;
+            }
+          }
+        });
+      },
+      // 表格合并行
+      objectSpanMethod({ row, column, rowIndex, columnIndex }) {
+        if(!this.tableautomerge||!this.tableautomerge.length){
+          return {
+              rowspan: 1,
+              colspan: 1,
+          };
+        }
+        // 计算一共有几列数据
+        let arraynum = Object.keys(this.tableData1[0]).length
+        for (let i = 0; i < arraynum; i++) {
+         let isarr =  this.tableautomerge.some(item=>item==Object.keys(row)[columnIndex])
+          if (columnIndex == i&&isarr) {
+            const _row = this.margeArray[i].Arr[rowIndex];
+            const _col = _row > 0 ? 1 : 0;
+            return {
+              rowspan: _row,
+              colspan: _col,
+            };
+          }
+        }
+      },
+      handleEdit(index, row) {
+      },
+      handleDelete(index, row) {
+      },
+    },
+  }
+</script>
+
+<style  lang="less" scoped>
+.menu-nav {
+  width: 100%;
+  display: flex;
+}
+.menu-operation {
+  display: flex;
+  justify-content: flex-end;
+}
+.mb-10{
+  margin-bottom: 10px;
+
+}
+</style>
---
+++ src/components/Application/table-auto-merge/table-auto-merge.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <TableAutoMergeAssembly 
+    :arrlist="arrlist" 
+    :data="data" 
+    :tableautomerge="tableautomerge" 
+    ></TableAutoMergeAssembly>
+  </div>
+</template>
+<script>
+import TableAutoMergeAssembly from './table-auto-merge-assembly.vue'
+export default {
+  components: {
+    TableAutoMergeAssembly
+  },
+  data() {
+      return {
+      data:[
+          {
+            id: 3,
+            type:'xmcl',
+            typeName:'项目成立',
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄',
+          },
+          {
+            id: 2,
+            type:'xmcl',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄',
+            typeName:'项目成立',
+            date: '2016-05-06',
+          },
+          {
+            id: 3,
+            type:'xmcl',
+            date: '2016-05-03',
+            name: '王小虎',
+            typeName:'项目成立',
+            address: '上海市普陀区金沙江路 15161 弄',
+          },
+          {
+            id: 4,
+            type:'xmzhqgl',
+            typeName:'项目中后期管理',
+            date: '2016-05-06',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄',
+          },
+          {
+            id: 5,
+            type:'htmq',
+            typeName:'合同面签',
+            date: '2016-05-06',
+            name: '王小虎2',
+            address: '上海市普陀区金沙江路 1516 弄',
+          },
+          {
+            id: 6,
+            type:'xmcl',
+            typeName:'项目成立',
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄',
+          },
+          {
+            id: 7,
+            type:'xmzhqgl',
+            typeName:'项目中后期管理',
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄',
+          },
+        ],
+        tableautomerge:['typeName','date','name','address'],
+        arrlist: [
+          {
+            key:"typeName",
+            label:' ',
+            align:'center',
+            width:'40',
+          },
+          {
+            key:"date",
+            label:"日期",
+            align:'center',
+            width:'180',
+          },
+          {
+            key:"name",
+            label:"姓名",
+            align:'center',
+          },
+          {
+            key:"address",
+            label:"地址",
+            align:'center',
+          }
+        ],
+    }
+  }
+}
+</script>
